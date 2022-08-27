02efaac9131fc5aa47183b8c22880b93e4308e85
ijoanet <jramos@thehotelsnetwork.com>
2022-08-27 17:30:40 +0200

    feat(iteration 2): about component and pass through variables via template

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,19 @@
       alt="Vue logo"
       src="./assets/logo.png"
     >
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <AboutComponent
+      about-me="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate sodales metus in accumsan. Suspendisse sagittis orci in enim consequat vulputate. Sed porta pretium nulla at pretium. Vivamus volutpat congue velit et molestie. Pellentesque id viverre mauris. Etiam est mauris, sagittis sit amet pharetra sit amet, ullamcorper sit amet nisi. Duis vestibulum magna sapien, a tempor massa dictum sed. Duis non aliquam odio. Fusce neque quam, dignissim a diam sed, semper interdum ante. Nullam viverra nulla efficitur, sagittis nunc a, ornare massa. Praesent blandit volutpat est a varius."
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import AboutComponent from './components/AboutComponent.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    AboutComponent,
   },
 };
 </script>
---
+++ src/components/AboutComponent.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="about">
+    <p>This is the 'aboutMe' content</p>
+    <p>{{ aboutMe }}</p>
+    <p>And this is Jesus just dacing</p>
+    <img src="https://i.pinimg.com/originals/3c/bc/58/3cbc5852b03e549f6ff4d14c80031349.gif">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AboutComponent',
+  props: {
+    aboutMe: {
+      type: String,
+      default: '😢There\'s nothing to talk about!😢',
+    },
+  },
+};
+
+</script>
+
+<style scoped>
+#about {
+    width: 70%;
+    margin: auto;
+}
+p:nth-child(1), p:nth-child(3) {
+  font-weight: 700;
+}
+p:nth-child(2) {
+    text-align: justify;
+}
+</style>
